36425e04315c8450ca4dbf52c72bac74ab7c8b21
Suhan Moon <leader@trabricks.io>
2021-05-01 12:54:59 +0900

    init commit.

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,83 @@
-import { KakaoSDK } from './types';
+import { AccessTokenType, KakaoSDK } from './types';
 
-export const init = async () => {};
+const getKakaoSDK = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    if (typeof window === 'undefined') reject({ message: 'unsupported platform' });
+    // @ts-ignore
+    if (typeof window['Kakao'] !== 'undefined') resolve(window['Kakao']);
+    var jsapi = document.createElement('script');
+    jsapi.type = 'text/javascript';
+    jsapi.src = 'https://developers.kakao.com/sdk/js/kakao.min.js';
+    var s = document.getElementsByTagName('script')[0];
+    s?.parentNode?.insertBefore(jsapi, s);
+    jsapi.onload = () => {
+      // @ts-ignore
+      resolve(window['Kakao']);
+    };
+    jsapi.onerror = reject;
+    jsapi.onabort = reject;
+  });
+};
+
+const getToken = (Kakao: any): AccessTokenType => {
+  const output = Kakao.Auth.getAccessToken();
+  return {
+    access_token: output.access_token,
+    refresh_token: output.refresh_token,
+    access_token_expiresAt: output.access_token_expiresAt,
+    refresh_token_expiresAt: output.refresh_token_expiresAt,
+    scopes: output.scopes,
+  };
+};
+
+export const init = async (appKey: string) => {
+  const Kakao = await getKakaoSDK();
+  Kakao.init(appKey);
+};
 
 export const login = async () => {
-  return undefined;
+  const Kakao = await getKakaoSDK();
+  const scope = 'account_email,gender';
+  const exec = () => new Promise((success, fail) => Kakao.Auth.login({ scope, success, fail }));
+  console.log(await exec());
+  return getToken(Kakao);
 };
 
-export const loginWithNewScopes = async (_scopes: string[]) => {
-  return undefined;
+export const loginWithNewScopes = async (scopes: string[]) => {
+  const Kakao = await getKakaoSDK();
+  const scope = scopes?.join(',');
+  const exec = () => new Promise((success, fail) => Kakao.Auth.login({ scope, success, fail }));
+  console.log(await exec());
+  return getToken(Kakao);
 };
 
-export const logout = async () => {};
+export const logout = async () => {
+  const Kakao = await getKakaoSDK();
+  if (!Kakao.Auth.getAccessToken()) throw { message: 'Not logged in.' };
 
-export const unlink = async () => {};
+  const exec = () => new Promise(success => Kakao.Auth.logout(success));
+  await exec();
+};
+
+export const unlink = async () => {
+  const Kakao = await getKakaoSDK();
+  const url = '/v1/user/unlink';
+  const exec = () => new Promise((success, fail) => Kakao.API.request({ url, success, fail }));
+  await exec();
+};
 
 export const getAccessToken = async () => {
-  return undefined;
+  const Kakao = await getKakaoSDK();
+  return getToken(Kakao);
 };
 
-export const getProfile = async () => {};
+export const getProfile = async () => {
+  const Kakao = await getKakaoSDK();
+  const url = '/v2/user/me';
+  const exec = () => new Promise((success, fail) => Kakao.API.request({ url, success, fail }));
+  console.log(await exec());
+  return {};
+};
 
 const app: KakaoSDK = {
   init,
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,62 @@
+export interface ProfileType {
+  id: number;
+  connected_at: string;
+  kakao_account: {
+    profile_needs_agreement?: boolean;
+    profile?: {
+      nickname?: string;
+      profile_image_url?: string;
+      thumbnail_image_url?: string;
+    };
+
+    emailNeedsAgreement?: boolean;
+    email?: string;
+    isEmailValid?: boolean;
+    isEmailVerified?: boolean;
+
+    birthdayNeedsAgreement?: boolean;
+    birthday?: string;
+
+    birthyearNeedsAgreement?: boolean;
+    birthyear?: string;
+
+    genderNeedsAgreement?: boolean;
+    gender?: 'male' | 'female' | null;
+
+    ciNeedsAgreement?: boolean;
+    ci?: string;
+    ciAuthenticatedAt?: string;
+
+    legalBirthDateNeedsAgreement?: boolean;
+    legalBirthDate?: string;
+
+    legalGenderNeedsAgreement?: boolean;
+    legalGender?: 'male' | 'female' | null;
+
+    legalNameNeedsAgreement?: boolean;
+    legalName?: string;
+
+    ageRangeNeedsAgreement?: boolean;
+    ageRange?:
+      | '0~9'
+      | '10~14'
+      | '15~19'
+      | '20~29'
+      | '30~39'
+      | '40~49'
+      | '50~59'
+      | '60~69'
+      | '70~79'
+      | '80~89'
+      | '90~'
+      | null;
+
+    phoneNumberNeedsAgreement?: boolean;
+    phoneNumber?: string;
+  };
+  properties: any;
+}
+
 export interface AccessTokenType {
   access_token: string;
   refresh_token: string;
