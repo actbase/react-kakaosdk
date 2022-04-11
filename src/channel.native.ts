d2322f7ad701d452dfc6235ebb3ef29090931190
Suhan Moon <leader@trabricks.io>
2022-04-12 01:17:45 +0900

    V3 모드로 변경중 (JS SDK 룩)

--- src/channel.native.ts
+++ src/channel.native.ts
@@ -4,19 +4,26 @@ import { NativeModules, TouchableWithoutFeedback } from 'react-native';
 
 const { RNAKakaoSDK } = NativeModules;
 
+const addChannel = async (props: IChannelBaseProps) => {
+  return await RNAKakaoSDK.channel_addChannel(props.channelPublicId);
+};
+
 const createAddChannelButton = (props: IChannelDefaultProps): React.FC => {
   if (props.container) {
     console.warn('container field is Web Only.');
   }
   return subProps =>
     React.createElement(TouchableWithoutFeedback, {
-      onPress: () => addChannel({ channelPublicId: props.channelPublicId }),
+      onPress: () => {
+        alert('a');
+        addChannel({ channelPublicId: props.channelPublicId });
+      },
       ...subProps,
     });
 };
 
-const addChannel = async (props: IChannelBaseProps) => {
-  return await RNAKakaoSDK.channel_addChannel(props.channelPublicId);
+const chat = async (props: IChannelBaseProps) => {
+  return await RNAKakaoSDK.channel_chat(props.channelPublicId);
 };
 
 const createChatButton = (props: IChannelDefaultProps): React.FC => {
@@ -28,10 +35,6 @@ const createChatButton = (props: IChannelDefaultProps): React.FC => {
       onPress: () => chat({ channelPublicId: props.channelPublicId }),
       ...subProps,
     });
-};
-
-const chat = async (props: IChannelBaseProps) => {
-  return await RNAKakaoSDK.channel_chat(props.channelPublicId);
 };
 
 const Channel: IKakaoChannel = {
