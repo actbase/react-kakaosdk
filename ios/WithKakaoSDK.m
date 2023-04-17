a4f56c93db34a8b0e087e4c2107861b4ec277acd
Suhan Moon <leader@actbase.io>
2023-04-17 15:44:23 +0900

    11

--- ios/WithKakaoSDK.m
+++ ios/WithKakaoSDK.m
@@ -1,14 +1,16 @@
 #import "WithKakaoSDK.h"
-#import "RNAKakaoSDK-Swift.h"
+#import <RNAKakaoSDK/RNAKakaoSDK-Swift.h>
 
 @implementation WithKakaoSDK
 
 + (BOOL)isKakaoTalkLoginUrl:(NSURL *)url {
-    return [RNAKakaoSDK isKakaoTalkLoginUrl: url];
+    return [RNAKakaoSDK isKakaoTalkLoginUrl:url];
 }
 
 + (BOOL)handleOpenUrl:(NSURL *)url {
-    return [RNAKakaoSDK handleOpenUrl: url];
+    return [RNAKakaoSDK handleOpenUrl:url];
+
 }
+
 
 @end
